a620dd1e3984d561a3ff67ee501e134307b01344
Antonio <johndoe@example.com>
2022-12-05 16:56:36 +0100

    añadiendo archivos

---
+++ ejercicio1.js
@@ -0,0 +1,6 @@
+document.title="Antonio David Navarro Jimenez";
+document.getElementById("titulo").innerHTML="Antonio David Navarro Jimenez";
+var section=document.getElementById("noticias");
+var oculta=section.getElementsByClassName("fila");
+let no=document.querySelectorAll('article:last-child');
+alert(no);
